78fe4ab11755cee2629d4c4964ee28634d60ea58
Richard Pfab <richard@squarepeg.co.za>
2017-10-03 09:37:02 +0200

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    to: 'richard',
-    text: 'helo dick'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new client connected');
 
-  socket.emit('newMessage', {
-    from: 'dick',
-    text: 'helo',
-    createdAt: 0
-  });
-
   socket.on('createMessage', (message) => {
     console.log('on createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
